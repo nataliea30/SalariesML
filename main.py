65ce3271dcc63790879c44b4f29443847701750b
nataliea30 <natalieanwar@outlook.com>
2023-07-05 23:26:45 -0400

    Transforming all data to numerical, converting all data types to int64, and building test and train sets

--- main.py
+++ main.py
@@ -9,3 +9,24 @@ dataFrame.drop(dataFrame[['salary','salary_currency']], axis = 1, inplace = True
 
 print(dataFrame.shape)
 print(dataFrame.head())
+
+
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+
+# Transform data using LabelEncoder to all be numerical
+cols = ['experience_level', 'employment_type', 'job_title','employee_residence','company_location','company_size']
+dataFrame[cols]=dataFrame[cols].apply(LabelEncoder().fit_transform)
+
+# Convert all data types to int64
+dataFrame = dataFrame.astype('int64')
+
+# Create X, y
+X = dataFrame.drop(["salary_in_usd"], axis=1)
+y = dataFrame["salary_in_usd"]
+
+# Build train and test sets
+# It is important to note that stratification is not possible, however oversampling the minority/undersampling the majority class can be done
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+dataFrame.info()
